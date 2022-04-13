e79516bebe8cf5ec5bfa1dab035bb451edc8978e
RODRIGO DE SOUSA DA CUNHA <RM339504@fiap.com.br>
2022-04-12 23:00:14 -0300

    Add project files.

---
+++ consoleRedis_Fiap/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+
+string connectionString = "20.225.241.127";
+var redis = ConnectionMultiplexer.Connect(connectionString);
+
+var pub = redis.GetSubscriber();
+
+var sub = redis.GetSubscriber();
+
+var dbCont = redis.GetDatabase();
+
+
+
+//sub.Subscribe("22net").OnMessage(
+//    mbox =>
+//    {
+//        Console.WriteLine(mbox.Message);
+//    });
+
+sub.Subscribe("perguntas").OnMessage(
+    mbox =>
+    {
+        Console.WriteLine(mbox.Message);
+
+        var x = mbox.Message;
+
+
+        //string a = "str123";
+        string b = string.Empty;
+        int val;
+        bool exit = false; 
+
+        for (int i = 0; i < x.ToString().Length; i++)
+        {
+            
+            if (Char.IsDigit(x.ToString()[i]))
+            {
+                b += x.ToString()[i];
+            }
+
+
+            if (Char.IsDigit(x.ToString()[i]).Equals("+"))
+                break;
+        }
+
+
+        if (b.Length > 0)
+        {
+            val = int.Parse(b);
+            var resp = val + val;
+
+            string[] soma = { "P1", "DevsIOBot", resp.ToString() };
+            var hash = new HashSet<string>(soma);
+
+            dbCont.HashSet("P1", "DevsIOBot", "5");
+        }
+           
+        
+
+       
+
+
+    });
+
+
+Console.ReadLine();
